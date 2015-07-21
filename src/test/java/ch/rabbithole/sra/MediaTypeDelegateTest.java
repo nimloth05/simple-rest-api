c1b8bf9dffb60117a8d5ab407b99b249a5e89253
nimloth05 <morpheus@rabbithole.ch>
2015-07-21 09:11:35 +0200

    - test for media type

---
+++ src/test/java/ch/rabbithole/sra/MediaTypeDelegateTest.java
@@ -0,0 +1,36 @@
+package ch.rabbithole.sra;
+
+import com.sun.ws.rs.ext.MediaTypeDelegate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.MediaType;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * TODO JavaDoc
+ */
+public final class MediaTypeDelegateTest {
+
+  private MediaTypeDelegate delegate;
+
+  @Before
+  public void setUp() {
+    delegate = new MediaTypeDelegate();
+  }
+
+  @Test
+  public void testFromString() {
+    MediaType mediaType = delegate.fromString(MediaType.APPLICATION_JSON);
+    assertEquals(MediaType.APPLICATION_JSON_TYPE, mediaType);
+  }
+
+  @Test
+  public void testToString() {
+    String asString = delegate.toString(MediaType.APPLICATION_JSON_TYPE);
+    assertEquals(MediaType.APPLICATION_JSON, asString);
+  }
+
+}
